6480fa3dcf8c7a15cc2eac92f6aa32332f05dc1a
Jeremy Morse <jgmorse@umich.edu>
2020-02-28 14:48:30 -0500

    Split isbns onto own orows; parse out isbns and formats into separate cells

--- bar2tmm.rb
+++ bar2tmm.rb
@@ -6,14 +6,32 @@ require 'csv'
 
 #tmm = CSV.open('data/TMM_Import_Template.csv', headers:true)
 
+def parse_format(str)
+  format = ''
+  str.match(/\((.*?)\)/) { format = $1 }
+  return format
+end
+
+def parse_isbn(str)
+  isbn = ''
+  str.match(/^(\d+)/) { isbn = $1}
+  return isbn
+end
+
+
 CSV.open('data/output.csv', 'w') do |output|
-  header = ['isbn','imprint']
+  header = ['isbn','format','imprint']
   output << header
   CSV.foreach(ARGV.shift, headers: true) do |input|
-    #ISBNs = row['ISBN(s)']
-    row = CSV::Row.new(header,[])
-    row['isbn'] = input['ISBN(s)']
-    row['imprint'] = 'British Archaeological Reports'
-    output << row
+    #e.g. 9781407333977 (ebook); 9781407303697 (paperback)
+    isbns_formats = input['ISBN(s)'].split('; ')
+    isbns_formats.each {|i|
+      row = CSV::Row.new(header,[])
+
+      row['isbn'] = parse_isbn(i)
+      row['format'] = parse_format(i)
+      row['imprint'] = 'British Archaeological Reports'
+      output << row
+    }
   end
 end
