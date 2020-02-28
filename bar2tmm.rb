e8ae75fce3c53a9b98b581fce81fbc0504054aaf
Jeremy Morse <jgmorse@umich.edu>
2020-02-28 18:11:07 -0500

    Author Bio

--- bar2tmm.rb
+++ bar2tmm.rb
@@ -145,7 +145,7 @@ CSV.open('data/output.csv', 'w') do |output|
       parse_creators( input['Creator(s)'], row )
 
       row['Language'] = 'English'
-
+      row['Author Bio'] = input['Author Blurb']
 
       row['Other ID'] = input['BAR Number']
       row['Other Subjects'] = input['Subject']
