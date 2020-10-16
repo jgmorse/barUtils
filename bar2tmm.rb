7e659abdc0a88f45aa08ff60c2c3102e8c12370d
Jeremy Morse <jgmorse@umich.edu>
2020-10-16 07:17:22 -0400

    ChildISBN, ChildMedia, ChildFormat

--- bar2tmm.rb
+++ bar2tmm.rb
@@ -35,7 +35,7 @@ def assign_products(row, input, pubYear)
 
 end
 
-def parse_format(str, row)
+def parse_format(str)
   media =''
   format = ''
   str.match(/\((.*?)\)/) { format = $1 }
@@ -51,16 +51,29 @@ def parse_format(str, row)
       format = 'Hardcover'
   end
 
-  row['Media'] = media
-  row['Format'] = format
+  return [media, format]
 end
 
-def parse_isbn(str, row, all_isbns_formats)
+def parse_isbn_format(this_isbn_format, row, all_isbns_formats)
+  #Get the isbn for this row
   isbn = ''
-  str.match(/^(\d+)/) { isbn = $1}
+  this_isbn_format.match(/^(\d+)/) { isbn = $1}
   row['ISBN13'] = isbn
-  #child_isbn_format = all_isbns_formats.delete(isbn).shift
-  #child_isbn_format.match(/^(\d+)/) { row['ChildISBN'] = $1}
+  m_f = parse_format(this_isbn_format)
+  row['Media'] = m_f[0]
+  row['Format'] = m_f[1]
+  #This is goofy: recall that we're in an 'each' loop of all isbn/formats for this title.
+  #The following block is how we get the isbn/format we're *NOT* currently operating on in the 'each' loop.
+  #This only works because we know BAR will only have 2 isbn/formats per title.
+  #Still, there's probably a better way to write this.
+  #Make a copy because we will need the deleted value later in this loop....
+  copy = all_isbns_formats.slice(0 .. -1)
+  copy.delete(this_isbn_format)
+  child_isbn_format = copy.shift
+  child_isbn_format.match(/^(\d+)/) { row['ChildISBN'] = $1}
+  m_f = parse_format(child_isbn_format)
+  row['ChildMedia'] = m_f[0]
+  row['ChildFormat'] = m_f[1]
 end
 
 def parse_creators(creators, row)
@@ -109,8 +122,9 @@ header = [
   'grouplevel2',      #required (Division)
   'grouplevel3',      #required (Imprint)
   'Media',            #required
-  'Format',
-  'ChildFormat',          #required
+  'Format',           #required
+  'ChildMedia',       #required
+  'ChildFormat',      #required
   'Page Count',
   'Trim Width',
   'Trim Length',
@@ -169,19 +183,17 @@ CSV.open('data/output.csv', 'w') do |output|
     isbns_formats = input['ISBN(s)'].split('; ')
     isbns_formats.each {|i|
       row = CSV::Row.new(header,[])
-      parse_isbn(i, row, isbns_formats)
+      parse_isbn_format(i, row, isbns_formats)
       row['Title'] = input['Title']
       row['Subtitle'] = input['Sub-Title']
       row['grouplevel1'] = 'Michigan Publishing'
       row['grouplevel2'] = input['Copyright Holder']
       row['grouplevel3'] = 'British Archaeological Reports'
       row['Publisher'] = 'Fulcrum Hosted Clients'
-
-      parse_format(i, row)
-
-
       row['BISAC Status'] = 'Active'
 
+      #WARNING!!!
+      #BAR have switched btw USA and Euro date formatting in the past....
       #parse European-format date
       pubDate = Date.strptime(input['Pub Date'].strip, "%m/%d/%Y")
       row['Pub Date'] = pubDate.strftime('%m/%d/%Y')
